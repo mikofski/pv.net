d4e3f7ef96109849d76c2d9aa3b1b1b1f10de556
Mark Mikofski <bwana.marko@yahoo.com>
2020-04-07 15:47:38 -0700

    remove extra unused imports

--- pv.net/Program.cs
+++ pv.net/Program.cs
@@ -6,10 +6,9 @@ namespace pv.net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            args = new string[4] { "19900101T12:30:00", "19900102T12:30:00", "19900103T12:30:00", "19900104T12:30:00" };
-            int nargs = args.Length;
-            SolarPosition sp = new pv.net.SolarPosition(args, (float)32.1, (float)-123.4);
+            Console.WriteLine("Calculate Simple Day Angle!");
+            string[] times = new string[4] { "19900101T12:30:00", "19900102T12:30:00", "19900103T12:30:00", "19900104T12:30:00" };
+            SolarPosition sp = new pv.net.SolarPosition(times, (float)32.1, (float)-123.4);
         }
     }
 }
