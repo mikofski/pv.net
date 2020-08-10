43c97b565e4c44f25a580dd84fb6040404695136
Mark Mikofski <bwana.marko@yahoo.com>
2020-08-09 17:57:54 -0700

    fix python tests, ignore vscode

--- test_pv.py
+++ test_pv.py
@@ -15,24 +15,28 @@ assy = clr.AddReference(str(src))
 from clr import pv
 
 lat, lon = 37.81, -122.25
+tz = -8.0
 
 dates = ["19900101T12:30:00", "19900102T12:30:00", "19900103T12:30:00", "19900104T12:30:00"]
-sp = pv.SolarPosition(dates, lat, lon)
-eot_test = sp.EquationOfTimeSpencer71()
+print('Solar Position')
+sp = pv.SolarPosition(dates, lat, lon, tz)
+print('Day Angle, offset=1')
+da = sp.CalcSimpleDayAngleArray()
+eot = sp.EquationOfTimeSpencer71(da)
 print('Equation of time, Spencer (1971)')
 for doy in range(4):
-    print(f'{doy+1:d} --> {eot_test[doy]:g}')
+    print(f'{doy+1:d} --> {eot[doy]:g}')
 
-doy = np.arange(4)+1;
+doy = np.arange(4) + 1
 eot_pvcdrom = equation_of_time_pvcdrom(doy)
-eot = equation_of_time_spencer71(doy)
-assert np.allclose([_ for _ in eot_test], eot)
+eot_test = equation_of_time_spencer71(doy)
+assert np.allclose([_ for _ in eot], eot_test)
 
 decl = declination_spencer71(doy)
 decl_cooper = declination_cooper69(doy)
 
-ts = pd.DatetimeIndex(dates, tz='Etc/GMT+8')
-ha = hour_angle(ts, lon, eot)
+ts = pd.DatetimeIndex(dates, tz=f'Etc/GMT{int(tz):+d}')
+ha = hour_angle(ts, lon, eot_test)
 
-sp = get_solarposition(ts, latitude=lat, longitude=lon)
+sp_test = get_solarposition(ts, latitude=lat, longitude=lon)
 ze = solar_zenith_analytical(37.81*np.pi/180.0, ha*np.pi/180.0, decl)*180/np.pi
